fbdaea45fc52856b075092b218bcf5f31e458c88
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2023-11-03 14:46:24 +0800

    Change defaults

--- classes/class_physical_parameters.m
+++ classes/class_physical_parameters.m
@@ -8,24 +8,20 @@ classdef class_physical_parameters < handle
         kb = 8.6173*1e-5; %Boltzmann constant in eV K^-1;
 
         %setting up default condensate parameters, geometry, and tof
-        max_longitudinal_density = 75e6; %peak longitudinal density 75 atoms per microns
+        max_longitudinal_density = 50e6; %peak longitudinal density 50 atoms per microns
         omega = 2*pi*2e3; %x oscillator frequencies - transverse (s^-1)
-        %omega = 3e3; 
         scattering_length = 5.2e-9; %scattering length for interaction broadening
         
-        %by default, we have a condensate of length 100 microns, separated
+        %by default, we have a condensate of length 100 microns (in z-axis), separated
         %a distance 3 microns initially, and spread over x_{max} - x_{min} microns after
         %tof
         default_condensate_length = 100e-6;%100 microns
-        x_min = -50e-6; 
-        x_max = 50e-6;
+        x_min = -100e-6; 
+        x_max = 100e-6;
         default_separation_distance = 3e-6; %3 microns
         default_expansion_time = 15e-3; %15 ms
-        default_coupling_J = 0.5; %default tunnel coupling 2*pi*1Hz
-        %default_coupling_J = 1;
-
-        %default insitu density profile string 
-        default_insitu_density = 'InverseParabola';
+        default_coupling_J = 0; %default tunnel coupling 
+        default_insitu_density = 'BoxPotential';
         
     end
     
