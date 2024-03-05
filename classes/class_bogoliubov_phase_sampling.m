7f57a5f545585db9828ac5f8164a94b4d2ccc560
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-03-05 11:26:55 +0800

    Update class_bogoliubov_phase_sampling.m
    Include both positive and negative values of wavenumber

--- classes/class_bogoliubov_phase_sampling.m
+++ classes/class_bogoliubov_phase_sampling.m
@@ -18,7 +18,7 @@ classdef class_bogoliubov_phase_sampling < class_physical_parameters & handle
     end
 
     methods
-        %Generate the constructor
+        %1. Generate the constructor
         function obj = class_bogoliubov_phase_sampling(temperature, coupling_J, mean_density, condensate_length)
             % Run the parent class constructor
             obj = obj@class_physical_parameters();
@@ -44,7 +44,7 @@ classdef class_bogoliubov_phase_sampling < class_physical_parameters & handle
         end
 
 
-        %Stochastic sampling 
+        %2. Stochastic sampling 
         function phase_samples = generate_samples(obj, max_n_fourier, z_resolution, N_samples)
             if nargin < 4
                 N_samples = 1;
@@ -54,17 +54,24 @@ classdef class_bogoliubov_phase_sampling < class_physical_parameters & handle
             prefactor = sqrt(4*obj.m*obj.kb*obj.temperature*obj.condensate_length/(obj.mean_density*h^2));
             tunnel_coupling_term = 2*obj.m*(obj.condensate_length)^2*obj.coupling_strength_J/(pi*h);
             phase_samples = zeros(N_samples,z_resolution);
-            cosine_coeffs = zeros(N_samples, max_n_fourier);
-            sine_coeffs = zeros(N_samples, max_n_fourier);
+            cosine_coeffs = zeros(N_samples, 2*max_n_fourier);
+            sine_coeffs = zeros(N_samples, 2*max_n_fourier);
             for j = 1:N_samples
-                for n = 1:max_n_fourier
-                    random_num1 = abs(log(rand()));
-                    random_num2 = rand();
-                    denom = n^2 + tunnel_coupling_term;
-                    cosine_coeffs(j,n) = prefactor*sqrt(random_num1/denom)*sin(2*pi*random_num2);
-                    sine_coeffs(j,n) = prefactor*sqrt(random_num1/denom)*cos(2*pi*random_num2);
-                    for i = 1:z_resolution
-                        phase_samples(j,i) = phase_samples(j,i) + sqrt(random_num1/denom)*sin(2*pi*n*z_grid(i)/obj.condensate_length+2*pi*random_num2);
+                for n = -max_n_fourier:max_n_fourier
+                    if n ~= 0
+                        random_num1 = abs(log(rand()));
+                        random_num2 = rand();
+                        denom = n^2 + tunnel_coupling_term;
+                        if n<0
+                            cosine_coeffs(j,n+max_n_fourier+1) = prefactor*sqrt(random_num1/denom)*sin(2*pi*random_num2);
+                            sine_coeffs(j,n+max_n_fourier+1) = prefactor*sqrt(random_num1/denom)*cos(2*pi*random_num2);
+                        elseif n>0
+                            cosine_coeffs(j,n+max_n_fourier) = prefactor*sqrt(random_num1/denom)*sin(2*pi*random_num2);
+                            sine_coeffs(j,n+max_n_fourier) = prefactor*sqrt(random_num1/denom)*cos(2*pi*random_num2);
+                        end
+                        for i = 1:z_resolution
+                            phase_samples(j,i) = phase_samples(j,i) + sqrt(random_num1/denom)*sin(2*pi*n*z_grid(i)/obj.condensate_length+2*pi*random_num2);
+                        end
                     end
                 end
             end
@@ -73,5 +80,21 @@ classdef class_bogoliubov_phase_sampling < class_physical_parameters & handle
             obj.fourier_cosine_coeffs = cosine_coeffs;
             obj.fourier_sine_coeffs = sine_coeffs;
         end
+
+        %3. A function to coarse grain/interpolate a given phase sample
+        function coarse_fine_profile = coarse_fine_grain(obj, target_longitudinal_points, profile)
+            if nargin < 3
+                profile = obj.phase_samples;
+            end
+            nmb_longitudinal_points = size(profile,2);
+            [Z] = ndgrid(1:nmb_longitudinal_points)';
+            nmb_samples = size(profile,1);
+            coarse_fine_profile = zeros(nmb_samples,target_longitudinal_points);
+            for i = 1:nmb_samples
+                profile_interpolant = griddedInterpolant(Z,profile(i,:));
+                coarse_fine_profile(i,:) = profile_interpolant(linspace(1,nmb_longitudinal_points, target_longitudinal_points));
+            end
+    end
+
     end %end of methods
 end %end of class
