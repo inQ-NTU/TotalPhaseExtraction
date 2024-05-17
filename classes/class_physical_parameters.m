0a2a25ddb896c2362ad50413333ddb31a8a9566c
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-05-17 15:21:58 +0800

    Update class_physical_parameters.m

--- classes/class_physical_parameters.m
+++ classes/class_physical_parameters.m
@@ -11,18 +11,15 @@ classdef class_physical_parameters < handle
         max_longitudinal_density = 75e6; %peak longitudinal density 75 atoms per microns
         omega = 2*pi*2e3; %x oscillator frequencies - transverse (s^-1)
         scattering_length = 5.2e-9; %scattering length for interaction broadening
+
         
         %by default, we have a condensate of length 100 microns (in z-axis), separated
-        %a distance 3 microns initially, and spread over x_{max} - x_{min} microns after
-        %tof
+        %a distance 3 microns initially
         default_condensate_length = 100e-6;%100 microns
-        x_min = -60e-6; 
-        x_max = 60e-6;
         default_separation_distance = 3e-6; %3 microns
         default_expansion_time = 15e-3; %15 ms
         default_coupling_J = 0; %default tunnel coupling 
-        default_insitu_density = 'BoxPotential';
-        
+        default_insitu_density = 'Flat';
     end
     
 end
