6c2f984337b439513849b541a6ec63a7387fa578
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-07-03 13:02:32 +0800

    exp data analysis update
    Adding and updating scripts for experimental data analysis

--- exp_data_analysis/shaking_data_fede/scan7440_analysis.m
+++ exp_data_analysis/shaking_data_fede/scan7440_analysis.m
@@ -1,8 +1,8 @@
 clear all; close all
 addpath('../../classes/')
 addpath('../../plotting_func/')
-addpath('results_shaking/scan7436/')
-load('scan7436_exp_data_for_phase_extraction_all.mat')
+addpath('results_shaking/scan7440/')
+load('scan7440_exp_data_for_phase_extraction_all.mat')
 
 %load density ripple data
 density_ripple_all = data_structure_basic.density_profiles_full_stat;
@@ -15,7 +15,7 @@ t_tof = 11.2e-3;
 
 %set extraction parameter
 n_max_fourier = 20;
-idx_begin = 20; %The start of untruncated index
+idx_begin = 25; %The start of untruncated index
 idx_end = 64; %The end of untruncated index
 z_cut = z(idx_begin:idx_end);
 shift = (z_cut(end) - z_cut(1))/2-z_cut(end);
@@ -62,6 +62,9 @@ for i = 1:length(evol_time)
     std_sineCoeffs_all(i,:) = std(ext_sineCoeffs_all{i}, 1);
 end
 
+thermal_dr = density_ripple_all{1};
+mean_thermal_dr = mean(thermal_dr, 2);
+
 %analyze thermal state
 thermal_corr = class_1d_correlation(ext_com_phase_all{1});
 
@@ -73,14 +76,14 @@ g1_midcut = g1_corr(floor(length(z_cut)/2),:);
 %fit the cosine correlation
 z_cut = z_cut*1e6;
 fitfun = fittype( @(a,b,c,x)a*exp(-abs(x-c)/b));
-[fitted_curve,gof] = fit(z_cut(5:end-5)',g1_midcut(5:end-5)',fitfun);
+[fitted_curve,gof] = fit(z_cut',g1_midcut',fitfun);
 coeffvals = coeffvalues(fitted_curve);
 
 %extract the temperature
 hbar = 1.05457e-34; %hbar
 m = 86.909*1.66054e-27; %mass of Rb-87 in kg
 kb = 1.380649e-23; %Boltzmann constant
-n0 = mean(mean_density_cut); %mean density around 70 per microns
+n0 = mean(mean_density_cut);
 %n0 = 70e6;
 lambdaT = coeffvals(2)*1e-6; % thermal coherence length in meter
 
@@ -90,9 +93,9 @@ lambdaT_uncertainty = (confidence_interval(2,2) - confidence_interval(1,2))*1e-6
 temperature_uncertainty = (extracted_temperature/lambdaT)*lambdaT_uncertainty;
 
 %plotting
-idx_1 = 9;
-idx_2 = 15;
-idx_3 = 21;
+idx_1 = 13;
+idx_2 = 19;
+idx_3 = 26;
 refined_z = linspace(z_cut(1), z_cut(end), 100);
 evol_time = evol_time*1e3;
 
@@ -129,7 +132,7 @@ hold on
 plot(z_cut, avg_ext_com_phase_all(idx_1,:),'Color','red', 'LineWidth',1.1)
 xlabel('$z\; (\rm \mu m)$','Interpreter','latex')
 ylabel('$\phi_+(z)$', 'Interpreter','latex')
-xlim([-22,22])
+xlim([-20,20])
 ylim([-5,5])
 box on
 ax = gca;
@@ -141,8 +144,9 @@ plot(z_cut, avg_ext_com_phase_all(idx_1,:),'Color', 'red', 'LineWidth',1.1)
 hold on
 plot(z_cut, avg_ext_com_phase_all(idx_2,:),'-.',  'Color', 'red', 'LineWidth',1.1)
 plot(z_cut, avg_ext_com_phase_all(idx_3,:),'--', 'Color', 'red', 'LineWidth',1.1)
-xlim([-22,22])
-ylim([-3.2,3.2])
+xlim([-20,20])
+ylim([-2,2])
+legend('$t = 15\; \rm ms$', '$t = 30\; \rm ms$', '$t = 47.5\; \rm ms$', 'Interpreter', 'latex', 'FontSize', 10)
 xlabel('$z\; (\rm \mu m)$','Interpreter','latex')
 ylabel('$\langle\phi_+(z)\rangle$', 'Interpreter','latex')
 box on
@@ -158,9 +162,9 @@ hold on
 plot(refined_evol_time, spline(evol_time, avg_cosineCoeffs_all(:,1), refined_evol_time), 'Color','red', 'LineWidth',1.1)
 plot(evol_time, avg_cosineCoeffs_all(:,2)','.-', 'Color', 'blue', 'LineWidth',1.1, 'MarkerSize', 8)
 yline(0)
-ylim([-3,3])
+ylim([-2,2])
 xline(0, 'LineStyle', '--')
-xlim([-35,120])
+xlim([-15,53])
 ylabel('$\langle\rm{Re}[A_k]\rangle$', 'Interpreter','latex')
 xlabel('$t\; (\rm ms)$', 'Interpreter','latex')
 box on
@@ -178,7 +182,7 @@ yline(0)
 ylim([-3,3])
 xline(0, 'LineStyle', '--')
 legend('$k = 2\pi/L$', '', '$k = 4\pi/L$','', '', '', 'Interpreter', 'latex', 'FontSize', 11)
-xlim([-35,120])
+xlim([-15,53])
 ylabel('$\langle\rm{Im}[A_k]\rangle$', 'Interpreter','latex')
 xlabel('$t\; (\rm ms)$', 'Interpreter','latex')
 box on
@@ -186,4 +190,4 @@ ax = gca;
 ax.LineWidth = 1.1;
 title('$\mathbf{f}$','FontName','Times','Color','black','Units', 'normalized','Interpreter','latex','Position',[0.1,0.8]);
 
-set(h, 'FontName', 'Times', 'FontSize', 16)
+set(h, 'FontName', 'Times', 'FontSize', 16 )
