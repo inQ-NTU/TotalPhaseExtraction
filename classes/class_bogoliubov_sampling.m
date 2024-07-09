7bb1718f4839ea38cd2859f34bafb091536a5623
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-07-09 10:36:45 +0800

    Update class_bogoliubov_sampling.m
    Cleaning up

--- classes/class_bogoliubov_sampling.m
+++ classes/class_bogoliubov_sampling.m
@@ -1,9 +1,11 @@
 %This is a class to stochastically sample phase profiles in 1D Bose gas
 %interferometry experiment
 
-%The scheme is based on the reference below
+%The scheme is based on the references below:
 %Stimming, H. P., Mauser, N. J., Schmiedmayer, J., & Mazets, I. E. (2010). 
-% Fluctuations and stochastic processes in one-dimensional many-body quantum systems. PRL , 105(1), 015301.
+%Fluctuations and stochastic processes in one-dimensional many-body quantum systems. PRL , 105(1), 015301.
+%Stimming, H. P., Mauser, N. J., Schmiedmayer, J., & Mazets, I. E. (2011). 
+% Dephasing in coherently split quasicondensates. PRA, 83(2), 023618.
 
 classdef class_bogoliubov_sampling < class_physical_parameters & handle
     properties
@@ -12,9 +14,7 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
         condensate_length
         coupling_strength_J
         wavevec_k
-
-        %fourier_cosine_coeffs
-        %fourier_sine_coeffs
+        g_coupling
         phase_samples
         density_fluct_samples
     end
@@ -45,113 +45,48 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
             
             obj.temperature = temperature;
             obj.wavevec_k = 2*pi/obj.condensate_length;
+            obj.g_coupling = 2*obj.hbar*obj.omega*obj.scattering_length;
         end
 
 
         %2. Stochastic sampling
-
-        %Function to sample phase - updated version
-        function phase_samples = generate_phase_samples(obj, max_n_fourier, pixnumz, N_samples)
+        %Function to sample phase and density fluctuation - updated version
+        function [phase_samples, density_fluct_samples] = generate_fluct_samples(obj, max_n_fourier, pixnumz, N_samples)
             if nargin < 4
                 N_samples = 1;
             end
             
             %Define the grid and the sampling prefactors
             z_grid = linspace(-obj.condensate_length/2, obj.condensate_length/2, pixnumz);
-            overall_prefactor = sqrt(2*obj.kb*obj.temperature/(obj.mean_density*obj.condensate_length));
+            overall_prefactor_phase = sqrt(2*obj.kb*obj.temperature/(obj.mean_density*obj.condensate_length));
+            overall_prefactor_density = sqrt(2*obj.kb*obj.temperature*obj.mean_density/obj.condensate_length);
             tunneling_energy = 2*obj.coupling_strength_J*obj.hbar;
-            %interaction_energy = 2*obj.g_coupling*obj.mean_density;
+            interaction_energy = 2*obj.g_coupling*obj.mean_density;
 
             %Initialize phase samples
             phase_samples = zeros(N_samples, pixnumz);
-            
-            %Start sampling
-            for i = 1:N_samples
-                for n = -max_n_fourier:max_n_fourier
-                    if n~=0
-                        kn = obj.wavevec_k*n;
-                        Ekn = (obj.hbar*kn)^2/(2*obj.m)+tunneling_energy;
-                        random_num1 = abs(log(rand()));
-                        random_num2 = rand();
-                        for j = 1:pixnumz
-                            phase_samples(i,j) = phase_samples(i,j)+sqrt(random_num1/Ekn)*sin(kn*z_grid(j)+2*pi*random_num2);
-                        end
-                    end
-                end
-            end
-            phase_samples = overall_prefactor*phase_samples;
-            obj.phase_samples = phase_samples;
-        end
-
-        %Function to sample density fluctuation
-        function density_fluct_samples = generate_density_fluct_samples(obj, max_n_fourier, pixnumz, N_samples)
-            if nargin < 4
-                N_samples = 1;
-            end
-
-            %Define the grid and the sampling prefactors
-            z_grid = linspace(-obj.condensate_length/2, obj.condensate_length/2, pixnumz);
-            overall_prefactor = sqrt(2*obj.kb*obj.temperature/(obj.mean_density*obj.condensate_length));
-            tunneling_energy = 2*obj.coupling_strength_J*obj.hbar;
-            g_coupling = 2*obj.hbar*obj.omega*obj.scattering_length;
-            interaction_energy = 2*g_coupling*obj.mean_density;
-
-            %Initialize density fluctuation samples
             density_fluct_samples = zeros(N_samples, pixnumz);
-            
             %Start sampling
             for i = 1:N_samples
                 for n = -max_n_fourier:max_n_fourier
                     if n~=0
                         kn = obj.wavevec_k*n;
-                        Ekn = ((obj.hbar*kn)^2)/(2*obj.m)+tunneling_energy+interaction_energy;
+                        Ekn_phase = (obj.hbar*kn)^2/(2*obj.m)+tunneling_energy;
+                        Ekn_density = (obj.hbar*kn)^2/(2*obj.m)+tunneling_energy+interaction_energy;
                         random_num1 = abs(log(rand()));
                         random_num2 = rand();
                         for j = 1:pixnumz
-                            density_fluct_samples(i,j) = density_fluct_samples(i,j)+sqrt(random_num1/Ekn)*sin(kn*z_grid(j)+2*pi*random_num2);
+                            phase_samples(i,j) = phase_samples(i,j)+sqrt(random_num1/Ekn_phase)*sin(kn*z_grid(j)+2*pi*random_num2);
+                            density_fluct_samples(i,j) = density_fluct_samples(i,j)+sqrt(random_num1/Ekn_density)*cos(kn*z_grid(j)+2*pi*random_num2);
                         end
                     end
                 end
             end
-            density_fluct_samples = overall_prefactor*obj.mean_density*density_fluct_samples;
-            obj.phase_samples = density_fluct_samples;
+            phase_samples = overall_prefactor_phase*phase_samples;
+            density_fluct_samples = overall_prefactor_density*density_fluct_samples;
+            obj.phase_samples = phase_samples;
+            obj.density_fluct_samples = density_fluct_samples;
         end
-        %Version 15 May 2024
-        %function phase_samples = generate_phase_samples(obj, max_n_fourier, z_resolution, N_samples)
-        %    if nargin < 4
-        %        N_samples = 1;
-        %    end
-        %    z_grid = linspace(-obj.condensate_length/2, obj.condensate_length/2, z_resolution);
-        %    h = obj.hbar*2*pi; 
-        %    prefactor = sqrt(4*obj.m*obj.kb*obj.temperature*obj.condensate_length/(obj.mean_density*h^2));
-        %    tunnel_coupling_term = 2*obj.m*(obj.condensate_length)^2*obj.coupling_strength_J/(pi*h);
-        %    phase_samples = zeros(N_samples,z_resolution);
-        %    cosine_coeffs = zeros(N_samples, 2*max_n_fourier);
-        %   sine_coeffs = zeros(N_samples, 2*max_n_fourier);
-        %    for j = 1:N_samples
-        %        for n = -max_n_fourier:max_n_fourier
-        %            if n ~= 0
-        %                random_num1 = abs(log(rand()));
-        %                random_num2 = rand();
-        %                denom = n^2 + tunnel_coupling_term;
-        %                if n<0
-        %                    cosine_coeffs(j,n+max_n_fourier+1) = prefactor*sqrt(random_num1/denom)*sin(2*pi*random_num2);
-        %                    sine_coeffs(j,n+max_n_fourier+1) = prefactor*sqrt(random_num1/denom)*cos(2*pi*random_num2);
-        %                elseif n>0
-        %                    cosine_coeffs(j,n+max_n_fourier) = prefactor*sqrt(random_num1/denom)*sin(2*pi*random_num2);
-        %                    sine_coeffs(j,n+max_n_fourier) = prefactor*sqrt(random_num1/denom)*cos(2*pi*random_num2);
-        %                end
-        %                for i = 1:z_resolution
-        %                    phase_samples(j,i) = phase_samples(j,i) + sqrt(random_num1/denom)*sin(2*pi*n*z_grid(i)/obj.condensate_length+2*pi*random_num2);
-        %                end
-        %            end
-        %        end
-        %    end
-        %    phase_samples = phase_samples*prefactor; %Multiplying temperature dependent prefactor
-        %    obj.phase_samples = phase_samples;
-        %    obj.fourier_cosine_coeffs = cosine_coeffs;
-        %    obj.fourier_sine_coeffs = sine_coeffs;
-        %end
 
         %3. A function to coarse grain/interpolate a given phase sample
         function coarse_fine_profile = coarse_fine_grain(obj, target_longitudinal_points, profile)
