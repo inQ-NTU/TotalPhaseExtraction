a7b072f917d99c9f890cbccbeb33ac30c1946815
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-08-23 13:00:57 +0800

    Update class_bogoliubov_sampling.m

--- classes/class_bogoliubov_sampling.m
+++ classes/class_bogoliubov_sampling.m
@@ -1,11 +1,8 @@
-%This is a class to stochastically sample phase profiles in 1D Bose gas
-%interferometry experiment
-
-%The scheme is based on the references below:
-%Stimming, H. P., Mauser, N. J., Schmiedmayer, J., & Mazets, I. E. (2010). 
-%Fluctuations and stochastic processes in one-dimensional many-body quantum systems. PRL , 105(1), 015301.
-%Stimming, H. P., Mauser, N. J., Schmiedmayer, J., & Mazets, I. E. (2011). 
-% Dephasing in coherently split quasicondensates. PRA, 83(2), 023618.
+%This is a code to stochastically sample phase and density fluctuation of
+%1D Bose gases 
+%The code is based on stochastic Bogoliubov sampling following the convention of
+% reference below:
+% Petrov, D. S. (2003). Bose-Einstein condensation in low-dimensional trapped gases (Doctoral dissertation, Universiteit van Amsterdam).
 
 classdef class_bogoliubov_sampling < class_physical_parameters & handle
     properties
@@ -21,14 +18,14 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
 
     methods
         %1. Generate the constructor
-        function obj = class_bogoliubov_sampling(temperature, mean_density, condensate_length, coupling_J)
+        function obj = class_bogoliubov_sampling(temperature, mean_density_single_condensate, condensate_length, coupling_J)
             % Run the parent class constructor
             obj = obj@class_physical_parameters();
             % Set up some of the physics parameters, possibly to default
             if nargin < 2
                 obj.mean_density = obj.max_longitudinal_density;
             else
-                obj.mean_density = mean_density;
+                obj.mean_density = mean_density_single_condensate;
             end
 
             if nargin < 3
@@ -55,11 +52,8 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
             if nargin < 4
                 N_samples = 1;
             end
-            
             %Define the grid and the sampling prefactors
             z_grid = linspace(-obj.condensate_length/2, obj.condensate_length/2, pixnumz);
-            overall_prefactor_phase = sqrt(2*obj.kb*obj.temperature/(obj.mean_density*obj.condensate_length));
-            overall_prefactor_density = sqrt(2*obj.kb*obj.temperature*obj.mean_density/obj.condensate_length);
             tunneling_energy = 2*obj.coupling_strength_J*obj.hbar;
             interaction_energy = 2*obj.g_coupling*obj.mean_density;
 
@@ -71,19 +65,24 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
                 for n = -max_n_fourier:max_n_fourier
                     if n~=0
                         kn = obj.wavevec_k*n;
-                        Ekn_phase = (obj.hbar*kn)^2/(2*obj.m)+tunneling_energy;
-                        Ekn_density = (obj.hbar*kn)^2/(2*obj.m)+tunneling_energy+interaction_energy;
-                        random_num1 = abs(log(rand()));
-                        random_num2 = rand();
+                        Ekn = (obj.hbar*kn)^2/(2*obj.m);
+                        bogoliubov_spectrum = sqrt((Ekn+tunneling_energy)*(Ekn+tunneling_energy+interaction_energy));
+                        fkn_plus = sqrt(1/obj.condensate_length)*(bogoliubov_spectrum/(Ekn+tunneling_energy))^(1/2);
+                        fkn_minus = sqrt(1/obj.condensate_length)*(bogoliubov_spectrum/(Ekn+tunneling_energy))^(-1/2);
+                        thermal_occupation = (exp(bogoliubov_spectrum/(obj.kb*obj.temperature))-1)^(-1);
+                        
+                        z0 = randn; z1 = randn; 
+                        bn = sqrt(thermal_occupation/2)*(z0+1j*z1);
+                        
                         for j = 1:pixnumz
-                            phase_samples(i,j) = phase_samples(i,j)+sqrt(random_num1/Ekn_phase)*sin(kn*z_grid(j)+2*pi*random_num2);
-                            density_fluct_samples(i,j) = density_fluct_samples(i,j)+sqrt(random_num1/Ekn_density)*cos(kn*z_grid(j)+2*pi*random_num2);
+                            phase_term = sqrt(1/(4*obj.mean_density))*fkn_plus*bn*exp(1j*kn*z_grid(j));
+                            density_term = 1j*sqrt(obj.mean_density)*fkn_minus*bn*exp(1j*kn*z_grid(j));
+                            phase_samples(i,j) = phase_samples(i,j)+phase_term+conj(phase_term);
+                            density_fluct_samples(i,j) = density_fluct_samples(i,j)+density_term+conj(density_term);
                         end
                     end
                 end
             end
-            phase_samples = overall_prefactor_phase*phase_samples;
-            density_fluct_samples = overall_prefactor_density*density_fluct_samples;
             obj.phase_samples = phase_samples;
             obj.density_fluct_samples = density_fluct_samples;
         end
@@ -101,6 +100,25 @@ classdef class_bogoliubov_sampling < class_physical_parameters & handle
                 profile_interpolant = griddedInterpolant(Z,profile(i,:));
                 coarse_fine_profile(i,:) = profile_interpolant(linspace(1,nmb_longitudinal_points, target_longitudinal_points));
             end
+        end
+
+        %4. A function to analytically compute correlation function
+        function corr = corr_function(obj, max_fourier, pixnumz)
+            z_grid = linspace(-obj.condensate_length/2, obj.condensate_length/2, pixnumz);
+            interaction_energy = 2*obj.g_coupling*obj.mean_density;
+            phase_var = zeros(1, pixnumz);
+            for n = -max_fourier:max_fourier
+                if n~= 0
+                    kn = obj.wavevec_k*n;
+                    Ekn = (obj.hbar*kn)^2/(2*obj.m);
+                    bogoliubov_spectrum = sqrt(Ekn*(Ekn+interaction_energy));
+                    thermal_occupation = (exp(bogoliubov_spectrum/(obj.kb*obj.temperature))-1)^(-1);
+                    phase_var = phase_var + (bogoliubov_spectrum/Ekn)*(1-cos(kn.*z_grid))*(1+2*thermal_occupation);
+                end
+            end
+            prefactor = (2*obj.mean_density*obj.condensate_length)^(-1);
+            phase_var = phase_var*prefactor;
+            corr = exp(-phase_var/2);
         end
     end %end of methods
 end %end of class
