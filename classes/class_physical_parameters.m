94a3139eb28b95da60e06564d3149e3b6975b042
Taufiq Murtadho <62237031+taufiqmurtadho@users.noreply.github.com>
2024-08-25 08:13:33 +0800

    Update class_physical_parameters
    Renaming parameters

--- classes/class_physical_parameters.m
+++ classes/class_physical_parameters.m
@@ -8,18 +8,17 @@ classdef class_physical_parameters < handle
         kb = 8.6173*1e-5; %Boltzmann constant in eV K^-1;
 
         %setting up default condensate parameters, geometry, and tof
-        max_longitudinal_density = 75e6; %peak longitudinal density 75 atoms per microns
         omega = 2*pi*2e3; %x oscillator frequencies - transverse (s^-1)
         scattering_length = 5.2e-9; %scattering length for interaction broadening
-
         
         %by default, we have a condensate of length 100 microns (in z-axis), separated
-        %a distance 3 microns initially
+        %a distance 3 microns initially with default density 75 atoms per
+        %microns (single condensate density)
+        default_mean_density = 75e6; %default single condensate density 
         default_condensate_length = 100e-6;%100 microns
         default_separation_distance = 3e-6; %3 microns
         default_expansion_time = 15e-3; %15 ms
-        default_coupling_J = 0; %default tunnel coupling 
-        default_insitu_density = 'Flat';
+        default_tunneling_strength_J = 0; %default tunnel coupling 
     end
     
 end
